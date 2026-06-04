6a5b6f5efaf37effa899de6804e7bb4293f14014
prashant-bbipl <prashantbbipl@gmail.com>
2026-06-04 13:45:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -458,7 +458,7 @@ export default function App() {
                       <p>• Tab: {saveConfig.sheetName}</p>
                       <p>• Obj: {saveConfig.tableName}</p>
                       <p>• File: {saveConfig.fileName}</p>
-                      <p>• Attachments Folder: {saveConfig.uploadFolderPath || saveConfig.fileName.replace(/\.xlsx?$/, '')}</p>
+                      <p>• Attachments Folder: {saveConfig.uploadFolderPath || saveConfig.fileName?.replace(/\.xlsx?$/, '')}</p>
                     </div>
                   </div>
                 ) : (
